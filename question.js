a48fdaa98cb2ac5bb80c7ebd0a298930ab98c791
iislamibrahim <islam.ibrahim.22230944@commerce.helwan.edu.eg>
2024-10-30 03:15:05 +0300

    Add files via upload

---
+++ question.js
@@ -0,0 +1,36 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What html stands for",
+    answer: "A.Option 1",
+    options: ["A.Option 1", "B.Option 2", "C.Option 3", "D.Option 4 "],
+  },
+
+  {
+    numb: 2,
+    question: "What html stands for",
+    answer: "B.Option 2",
+    options: ["A.Option 1", "B.Option 2", "C.Option 3", "D.Option 4"],
+  },
+
+  {
+    numb: 3,
+    question: "What html stands for",
+    answer: "C.Option 3",
+    options: ["A.Option 1", "B.Option 2", "C.Option 3", "D.Option 4"],
+  },
+
+  {
+    numb: 4,
+    question: "What CSS stands for",
+    answer: "D.Option 4",
+    options: ["A.Option 1", "B.Option 2", "C.Option 3", "D.Option 4"],
+  },
+
+  {
+    numb: 5,
+    question: "What CSS stands for",
+    answer: "D.ayhaga",
+    options: ["A.Option 1", "B.Option 2", "C.Option 3", "D.ayhaga"],
+  },
+];
